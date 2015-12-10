1829c8516f42d569abee1a575ca26b5ea174d4dd
Justin Joyce <jjoyce07@yahoo.com>
2015-12-10 11:09:26 -0600

    Most Frequent Item in Array Solution

--- solutions.js
+++ solutions.js
@@ -29,3 +29,37 @@ for(i = 0; i < array.length; ++i) {
   return result
 }
 count(a);
+
+//******************************//
+
+// Write a program to find count of the most frequent item of an array.
+
+// Assume that input is array of integers.
+
+var numbers = [3, -1, -1, -1, 2, 3, -1, 3, -1, 2, 4, 9, 3];
+
+function mostFrequentItemCount(collection) {
+  if(collection.length == 0)
+    	return 0;
+    var modeMap = {};
+    var maxEl = collection[0], maxCount = 1;
+    for(var i = 0; i < collection.length; i++)
+    {
+    	var el = collection[i];
+    	if(modeMap[el] == null)
+    		modeMap[el] = 1;
+    	else
+    		modeMap[el]++;	
+    	if(modeMap[el] > maxCount)
+    	{
+    		maxEl = el;
+    		maxCount = modeMap[el];
+    	}
+    }
+    return modeMap[maxEl];
+}
+mostFrequentItemCount(numbers);
+
+
+
+
