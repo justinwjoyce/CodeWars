a0d1dca2ea0f554df8649b852a69b9f02f8dd705
Justin Joyce <jjoyce07@yahoo.com>
2015-12-14 10:34:52 -0600

    more solutions

--- solutions.js
+++ solutions.js
@@ -60,6 +60,19 @@ function mostFrequentItemCount(collection) {
 }
 mostFrequentItemCount(numbers);
 
+//better solution
+function mostFrequentItemCount(collection) {
+  if (collection.length === 0) return 0;
+  
+  var count = Object.create(null);
+  
+  collection.forEach( item => {
+    count[item] = (count[item] || 0) + 1;
+  });
+  
+  return Math.max(...Object.values(count));
+}
+
 
 
 
