ca7c4a77a60d6c329ecbf117c612bbf24f172f05
Justin Joyce <jjoyce07@yahoo.com>
2015-12-25 11:07:19 -0600

    close to answer

--- solutions.js
+++ solutions.js
@@ -133,6 +133,19 @@ function getNumberRange(first, last){
 
 //******************************//
 
+// Calculate the sum of all the arguments passed to a function.
+// Note: If any of the arguments is not a finite number the function should return false/False instead of the sum of the arguments.
+ function sum() {
+   var total = 0;
+   for (var i = 0; i < arguments.length; i++){
+     if ( isNaN( parseFloat(arguments[i]) ) ) {
+       return false;
+     } else {
+       total += parseFloat( arguments[i] );
+     }
+   }
+   return total;
+ }
 
 
 
