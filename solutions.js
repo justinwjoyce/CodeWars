f3ac2907728e08979256d8ba11926db6968c8ecd
Justin Joyce <jjoyce07@yahoo.com>
2016-01-06 14:56:31 -0600

    binary q & a

--- solutions.js
+++ solutions.js
@@ -147,6 +147,17 @@ function getNumberRange(first, last){
    return total;
  }
 
+//******************************//
+
+//Implement a function that successfully adds two numbers together and returns their solution in binary. The //conversion can be done before, or after the addition of the two.
+
+//The binary number returned should be a string!
+function addBinary(a,b) {
+    var sum = a + b
+    var binary = (sum >>> 0)
+    return binary.toString(2)
+}
+
 
 
 
