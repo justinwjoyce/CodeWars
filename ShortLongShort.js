b1f5405c588f5ae730ebbc2730a74958f046bbf2
Justin Joyce <jjoyce07@yahoo.com>
2016-01-17 14:46:15 -0600

    added short long short solution

---
+++ ShortLongShort.js
@@ -0,0 +1,15 @@
+// 8kyu
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length0).
+
+// For example:
+
+// solution("1", "22") // returns "1221"
+// solution("22", "1") // returns "1221"
+
+function solution(a, b) {
+  if (a.length < b.length) {
+  	return a + b + a;
+  } else {
+  	return b + a + b;
+  }
+}
