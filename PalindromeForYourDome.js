18c6d0b60eaece0eabbc219d72a49f97fd5ec508
Justin Joyce <jjoyce07@yahoo.com>
2016-01-18 10:16:43 -0600

    added counting power sets, is it even, and palindrome for your dome solutions

---
+++ PalindromeForYourDome.js
@@ -0,0 +1,23 @@
+// 6kyu
+// A palindrome is a word, phrase, number, or other sequence of symbols or elements, whose meaning may be interpreted the same way in either forward or reverse direction. Famous examples include "Amore, Roma", "A man, a plan, a canal: Panama" and "No 'x' in 'Nixon'". - wikipedia
+
+// Our goal is to determine whether or not a given string is a valid palindrome or not.
+
+// Like the above examples, here are a few test cases which are also populated:
+
+// "Amore, Roma" => valid
+// "A man, a plan, a canal: Panama" => valid
+// "No 'x' in 'Nixon'" => valid
+// "Abba Zabba, you're my only friend" => invalid
+// You can see that they are case insensitive and disregards non alphanumeric characters. In addition to a few predefined tests, your function will also be tested against a random string generator 50 times which are guaranteed to produce valid palindromes.
+
+function palindrome(string) {
+    string = string.replace(/\W|_/g, '').toLowerCase();
+
+    for (i = 0; i < Math.floor(string.length/2); i++) {  
+        if (string[i] !== string[string.length -1 - i]) {
+            return false;
+        }
+    }
+    return true;
+}
