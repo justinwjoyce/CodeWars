7ddadd373e633ae9f313006adbad0344c07ad735
Justin Joyce <jjoyce07@yahoo.com>
2016-02-03 08:50:53 -0600

    added find the remainder solution

---
+++ FindRemainder.js
@@ -0,0 +1,7 @@
+// 8kyu
+// Write a function that accepts two arguments and returns the remainder after dividing the larger number by the smaller number. 
+// Division by zero should return NaN. Arguments will both be integers.
+
+function remainder(a, b){
+  return a > b ? a % b : b % a;
+}
