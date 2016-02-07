1ad9a55769fe15b1ef672e2c30544de106a08b31
Justin Joyce <jjoyce07@yahoo.com>
2016-02-07 12:13:52 -0600

    added kata example twist solution

---
+++ KataExampleTwist.js
@@ -0,0 +1,12 @@
+// 8kyu
+// This is an easy twist to the example kata (provided by Codewars when learning how to create your own kata).
+
+// Add the value "codewars" to the websites array 1,000 times.
+
+// var websites = []
+
+// add the value "codewars" to the websites array 1,000 times
+var websites = [];
+while (websites.length < 1000) {
+  websites.push("codewars");
+}
