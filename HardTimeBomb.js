d092401b587571272851d4b68a7e267a65b03c8a
Justin Joyce <jjoyce07@yahoo.com>
2016-02-09 14:39:08 -0600

    added hard time bomb solution

---
+++ HardTimeBomb.js
@@ -0,0 +1,9 @@
+// 3kyu
+// A bomb has been set to go off! You have to find the wire and cut it in order to stop the timer. There is a global var
+// that holds the numeric ID to which wire to cut. Find that and then you can Bomb.CutTheWire(wireKey);
+ 
+for (var key in global) {
+    if (key.slice(0, 4) === 'boom') 
+    var wireCode = global[key];
+}
+Bomb.CutTheWire(wireCode);
