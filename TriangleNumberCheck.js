83b67ae59fe8bfdadb4ec5dfb2e69f06cc997619
Justin Joyce <jjoyce07@yahoo.com>
2016-02-14 11:47:40 -0600

    added triangle number check solution

---
+++ TriangleNumberCheck.js
@@ -0,0 +1,43 @@
+// 6kyu
+// A triangle number is a number where n objects form an equilateral triangle (it's a bit hard to explain). For example, 6 is a triangle number because you can arrange 6 objects into an equilateral triangle:
+
+//   1
+//  2 3
+// 4 5 6
+// 8 is not a triangle number because 8 objects do not form an equilateral triangle:
+
+//    1
+//   2 3
+//  4 5 6
+// 7 8
+// In other words, the nth triangle number is equal to the sum of the n natural numbers from 1 to n.
+
+// Your task:
+
+// Check if a given input is a valid triangle number. Return true if it is, false if it is not (note that any non-integers, including non-number types, are not triangle numbers).
+
+// You are encouraged to develop an effective algorithm: test cases include really big numbers.
+
+// Assumptions:
+
+// You may assume that the given input, if it is a number, is always positive.
+
+// Notes:
+
+// 0 and 1 are triangle numbers.
+
+function isTriangleNumber(number) {
+  var num = 1;
+  if(number === 1 || number === 0) {
+    return true;
+  }
+  for(var i = 2; i <= number; i++) {
+    num += i;
+    if(num === number) {
+       return true;
+    }
+  }
+  return false;
+}
+
+
