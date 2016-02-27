25efe8e5c53b1dba89e7fd62bf7abd5f35f34bbf
Justin Joyce <jjoyce07@yahoo.com>
2016-02-27 10:44:25 -0600

    added baby steps solution

---
+++ ES6/BabySteps.js
@@ -0,0 +1,6 @@
+// beta
+// Jon has a string but its where the spaces (' ') should be there are commas (,).
+
+// Your task is to help Jon replace the commas with spaces.
+
+const commaReplacer = string => string.replace(/,/g, ' ');
