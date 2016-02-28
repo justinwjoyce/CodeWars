98f1dc84aaaaaa7d31bc413583e7065fcec00e65
Justin Joyce <jjoyce07@yahoo.com>
2016-02-27 19:49:41 -0600

    added barking mad solution

---
+++ BarkingMad.js
@@ -0,0 +1,20 @@
+// 8kyu
+// Teach snoopy and scooby doo how to bark using object methods. Currently only snoopy can bark and not scooby doo.
+
+// Use method prototypes to enable all Dogs to bark.
+
+function Dog (breed) {
+  this.breed = breed;
+}
+
+var snoopy = new Dog("Beagle");
+
+snoopy.bark = function() {
+  return "Woof";
+};
+
+var scoobydoo = new Dog("Great Dane");
+
+scoobydoo.bark = function() {
+  return "Woof";
+};
