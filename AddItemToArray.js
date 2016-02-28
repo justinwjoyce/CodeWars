9b07da6d1ca95311478e2cac96501142e296628b
Justin Joyce <jjoyce07@yahoo.com>
2016-02-27 23:07:53 -0600

    added add item to array solution

---
+++ AddItemToArray.js
@@ -0,0 +1,8 @@
+// 8kyu
+// Add the value "codewars" to the websites array.
+// After your code executes the websites array should == ["codewars"]
+
+// The websites array has already been defined for you using the following code:
+
+// var websites = [];
+websites[0] = "codewars";
