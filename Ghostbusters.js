ebac212c0418c24cb77db331127d120255bd682c
Justin Joyce <jjoyce07@yahoo.com>
2016-03-12 15:54:09 -0600

    added ghostbusters solution

---
+++ Ghostbusters.js
@@ -0,0 +1,23 @@
+// 7kyu
+// Oh no! Ghosts have reportedly swarmed the city. It's your job to get rid of them and save the day!
+
+// In this kata, strings represent buildings while whitespaces within those strings represent ghosts.
+
+// So what are you waiting for? Return the building(string) without any ghosts(whitespaces)!
+
+// Example:
+
+// ghostBusters("Sky scra per");
+// Should return:
+
+// "Skyscraper"
+// If the building contains no ghosts, return the string:
+
+// "You just wanted my autograph didn't you?"
+
+function ghostBusters(building) {
+if (/ /.test(building))
+    return building.replace(/\s/g, '');
+    
+  return `You just wanted my autograph didn't you?`;
+}
