38f832021df3655e542dbc8085681d9cfd773137
Justin Joyce <jjoyce07@yahoo.com>
2016-03-17 08:31:58 -0500

    forgot to add the solution

--- HowGoodAreYou.js
+++ HowGoodAreYou.js
@@ -1,1 +1,10 @@
-HowGoodAreYou.js
+// 8kyu
+// There was a test in your class and you passed it. Congratulations!
+// But you're an ambitious person. You want to know if you're better than the average student in your class.
+// You got an array with your colleges' points. Now calculate the average to your points!
+
+// Return True if you're better, else False!
+
+function betterThanAverage(classPoints, yourPoints) {
+  return yourPoints > classPoints.reduce((x, y) => x + y) / classPoints.length;
+}
