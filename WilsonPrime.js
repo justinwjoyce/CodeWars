acffe3e5488abee586c3b3775281ccc2ba175675
Justin Joyce <jjoyce07@yahoo.com>
2016-06-30 09:34:17 -0500

    added wilson prime solution

---
+++ WilsonPrime.js
@@ -0,0 +1,21 @@
+// 8kyu 
+
+// Wilson primes satisfy the following condition. Let P represent a prime number.
+
+// Then ((P-1)! + 1) / (P * P) should give a whole number.
+
+// Your task is to create a function that returns true if the given number is a Wilson prime.
+
+function amIWilson(prime) {
+  let r = ((factorial(prime-1) + 1) / (prime * prime));
+  return r === ~~r;
+}
+
+var f = [];
+function factorial (number) {
+  if (number == 0 || number == 1)
+    return 1;
+  if (f[number] > 0)
+    return f[number];
+  return f[number] = factorial(number-1) * number;
+}
