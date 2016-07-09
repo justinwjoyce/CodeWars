a0b10d5b388d8cbb6407167119a90e32bff398e2
Justin Joyce <jjoyce07@yahoo.com>
2016-07-09 10:05:52 -0500

    added best practice solution

--- HelloHappyCodevarrior.js
+++ HelloHappyCodevarrior.js
@@ -22,3 +22,15 @@ function Warrior(n){
 Warrior.prototype.toString = function(){
     return "Hi! my name's "+this.name();
 }
+ // best practice solution
+ function Warrior(n){
+  var name = n;
+  this.name = function(n){
+    if( n ) name=n;
+    return name;
+  }  
+}
+  
+Warrior.prototype.toString = function(){
+    return "Hi! my name's "+this.name();
+}
