dead085228a5f69e306d83b7414c0086df6ab1a8
Justin Joyce <jjoyce07@yahoo.com>
2016-07-14 22:11:13 -0500

    added grasshoppper summation solution

---
+++ ES6/GrasshopperSummation.js
@@ -0,0 +1,13 @@
+// 8kyu
+
+// Write a program that finds the summation of every number between 1 and num. The number will always be a positive integer greater than 0.
+
+// For example:
+
+// summation(2) -> 3
+// 1 + 2
+
+// summation(8) -> 36
+// 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8
+
+const summation = (num) => Array.apply(null, { length: num }).map((x, i) => i + 1).reduce((a, b) => a + b);
