3bec25cf78e5b84e3aafe10a3316bdc987300132
Justin Joyce <jjoyce07@yahoo.com>
2016-07-19 07:26:13 -0500

    added not very secure solution

---
+++ ES6/NotVerySecure.js
@@ -0,0 +1,12 @@
+// 8kyu 
+
+// In this example you have to validate if a user input string is alphanumeric. The given string is not nil, so you don't have to check that.
+
+// The string has the following conditions to be alphanumeric:
+
+// At least one character ("" is not valid)
+// Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
+// No whitespaces/underscore
+// The given solution is not bad, but it's still possible to trick the regular expression, can you figure out why?
+
+const alphanumeric = string => /^[a-z0-9]+$/i.test(string);
