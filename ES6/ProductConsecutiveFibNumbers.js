e041df1b9c6700e6c73a8f7f46324d788c940c2f
Justin Joyce <jjoyce07@yahoo.com>
2016-07-20 09:07:32 -0500

    added better solution

--- ES6/ProductConsecutiveFibNumbers.js
+++ ES6/ProductConsecutiveFibNumbers.js
@@ -51,3 +51,15 @@ let looping = function(n) {
     }
     return final;
 };
+
+// better solution
+
+function productFib(prod){
+  var n = 0;
+  var nPlus = 1;  
+  while(n*nPlus < prod) {
+    nPlus = n + nPlus;
+    n = nPlus - n;
+  }
+  return [n, nPlus, n*nPlus===prod];
+}
