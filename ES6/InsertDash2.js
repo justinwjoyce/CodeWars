421ae66dbceab625fe06b610deaa7fba4b764d7c
Justin Joyce <jjoyce07@yahoo.com>
2016-07-26 12:41:48 -0500

    added alternate solution to insertdash

--- ES6/InsertDash2.js
+++ ES6/InsertDash2.js
@@ -9,3 +9,11 @@
 // Zero shouldn't be considered even or odd.
 
 const insertDashII = num => `${num}`.replace(/([13579])(?=[13579])/g, "$1-").replace(/([2468])(?=[2468])/g, "$1*");
+
+// alternate solution
+
+function insertDashII(num) {
+  return String(num)
+    .replace(/([13579])(?=[13579])/g, "$1-")
+    .replace(/([2468])(?=[2468])/g, "$1*")
+}
