2342658b7c58c926df7f0683f6ab888aa04503d9
Justin Joyce <jjoyce07@yahoo.com>
2016-07-26 12:40:39 -0500

    added insert dash 2 solution

---
+++ ES6/InsertDash2.js
@@ -0,0 +1,11 @@
+// 7kyu
+
+// This is a follow up from my kata Insert Dashes. 
+// Write a function insertDashII(num) that will insert dashes ('-') between each two odd numbers and asterisk ('*') between each even numbers in num
+// For example: 
+// insertDashII(454793) --> 4547-9-3 
+// insertDashII(1012356895) --> 10123-56*89-5 
+
+// Zero shouldn't be considered even or odd.
+
+const insertDashII = num => `${num}`.replace(/([13579])(?=[13579])/g, "$1-").replace(/([2468])(?=[2468])/g, "$1*");
