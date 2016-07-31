f511b165e14d83fadb323a63831fff6677357fb4
Justin Joyce <jjoyce07@yahoo.com>
2016-07-31 13:50:22 -0500

    added punky brewsters socks solution

---
+++ PunkyBrewstersSocks.js
@@ -0,0 +1,25 @@
+// 7kyu
+
+// Punky loves wearing different colored socks, but Henry can't stand it.
+
+// Given an array of different colored socks, return a pair depending on who was picking them out.
+
+// Example:
+
+// getSocks('Punky', ['red','blue','blue','green']) -> ['red', 'blue']
+// Note that Punky can have any two colored socks, in any order, as long as they are different and both exist. Henry always picks a matching pair.
+
+// If there is no possible combination of socks, return an empty array.
+
+function getSocks(name, socks) {
+  if (name === 'Henry') {
+    let sock = socks.filter(s => socks.filter(n => n == s).length > 1)[0];
+    if (!sock) return [];
+    return [sock, sock];
+  }
+  else if (name === 'Punky') {
+    let sock = socks.filter(s => socks.filter(n => n == s).length == 1)[0];
+    if (!sock) return [];
+    return [sock, socks.filter(s => sock != s)[0]];
+  }
+}
