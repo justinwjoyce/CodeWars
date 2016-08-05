781ba58ef940e7f22ed606a8702c65ab78e69663
Justin Joyce <jjoyce07@yahoo.com>
2016-08-05 09:40:28 -0500

    added best practices solution

--- ES6/HelpALFFindHisSpaceship.js
+++ ES6/HelpALFFindHisSpaceship.js
@@ -32,3 +32,17 @@ function findSpaceship(map) {
   
   return spaceShip;
 }
+
+// best practices
+
+const findSpaceship = (map = "") => {
+  map = map.split("\n").reverse()
+
+  for (let y = 0; y < map.length; y++) {
+    for (let x = 0; x < map[y].length; x++) {
+      if (map[y][x] === "X") return [x, y]
+    }
+  }
+
+  return "Spaceship lost forever."
+}
