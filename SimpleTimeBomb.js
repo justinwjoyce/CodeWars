7a2368f1078e9fdc32bff5d22807b47f1fb19a6b
Justin Joyce <jjoyce07@yahoo.com>
2016-08-11 21:43:49 -0500

    added best practices solution

--- SimpleTimeBomb.js
+++ SimpleTimeBomb.js
@@ -3,3 +3,8 @@
 // A bomb has been set to go off! You have to find the wire and cut it in order to stop the timer. There is a global property that holds the key to which wire to cut. Find that and then you can CutTheWire(wireKey);
 
 CutTheWire(this[Object.keys(this).slice(-1)]);
+
+// best practices solution
+
+var wire = Object.keys(this).pop()
+CutTheWire(this[wire]);
