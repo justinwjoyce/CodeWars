197b71d9e35c13d1164f8fe1328e0da61bc57e8c
Justin Joyce <jjoyce07@yahoo.com>
2017-03-08 21:03:14 -0600

    added ruby solution

---
+++ Ruby/digitize.rb
@@ -0,0 +1,13 @@
+# Given a non-negative integer, return an array / a list of the individual digits in order.
+
+# Examples:
+
+# 123 => [1,2,3]
+
+# 1 => [1]
+
+# 8675309 => [8,6,7,5,3,0,9]
+
+def digitize(n)
+  n.to_s.split('').map(&:to_i)
+end
